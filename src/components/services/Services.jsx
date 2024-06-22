b55ffa6f8bd0fa24041c414f15d255243a89a2c8
Bhuwan Thapa <60286433+B1Th@users.noreply.github.com>
2024-06-22 11:40:23 +0545

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,29 +2,6 @@ import { useRef } from "react";
 import "./services.scss";
 import { animate, inView, motion, useInView } from "framer-motion";
 
-const boxItems = [
-  {
-    title: "Real-Time Monitoring",
-    description:
-      "EcoTrack is a web-based platform for real-time monitoring of environmental conditions. It provides users with live data on pollution levels from various global sensors.",
-  },
-  {
-    title: "HealthSync",
-    description:
-      "HealthSync is a web application that aggregates data from various fitness and health devices into a single, easy-to-use dashboard. Users can track their health goals.",
-  },
-  {
-    title: "LearnHub",
-    description:
-      "LearnHub is an interactive web portal designed for students and educators. It offers a wide range of online courses, tutorials, and educational resources across various subjects.",
-  },
-  {
-    title: "Smart Shopping Assistant",
-    description:
-      "ShopEase is a web-based smart shopping assistant that helps users find the best deals and compare prices across multiple online stores. It offers personalized recommendations feature.",
-  },
-];
-
 const variants = {
   initial: {
     x: -500,
@@ -75,18 +52,6 @@ const Services = () => {
           </h1>
           <button>What We Do?</button>
         </div>
-      </motion.div>
-      <motion.div className="listContainer" variants={variants}>
-        {boxItems.map((item, index) => (
-          <motion.div
-            className="box"
-            whileHover={{ background: "lightgray", color: "black" }}
-          >
-            <div className="title">{item.title}</div>
-            <div className="description">{item.description}</div>
-            <button>Go</button>
-          </motion.div>
-        ))}
       </motion.div>
     </motion.div>
   );
