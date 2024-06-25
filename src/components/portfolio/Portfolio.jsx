ac564fbb5b0575d2452748527991b6ff15be9eea
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-06-25 18:22:36 +0545

    added links to the project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
@@ -6,26 +6,30 @@ const items = [
   {
     id: 1,
     title: "Resume Parser And Ranker",
-    img: "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "/public/resumeparser.png",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ",
+    link: "https://creative-shortbread-02f6c5.netlify.app",
   },
   {
     id: 2,
     title: "Text To Image Generator",
-    img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img: "/public/imagegen.png",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ",
+    link: "https://monumental-gecko-f9f035.netlify.app",
   },
   {
     id: 3,
     title: "Currency Exchange Converter",
-    img: "https://images.pexels.com/photos/287240/pexels-photo-287240.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img: "https://github.com/B1Th/Currency-Exchange-Rate",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ",
+    link: "",
   },
   {
     id: 4,
     title: "Personal Portfolio Site",
-    img: "https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img: "/public/portfoliosite.png",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ",
+    link: "https://bhuwan-thapa.com.np",
   },
 ];
 
@@ -43,11 +47,14 @@ const Single = ({ item }) => {
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <motion.img
-              src={item.img}
-              alt=""
-              whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-            />
+            <a href={item.link} target="_blank">
+              <span class="tooltiptext">Tooltip message</span>
+              <motion.img
+                src={item.img}
+                alt=""
+                whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
+              />
+            </a>
           </div>
 
           <motion.div className="textContainer" style={{ y }}>
