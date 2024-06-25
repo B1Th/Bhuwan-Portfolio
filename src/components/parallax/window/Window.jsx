6433561639fd4bf6f24877ce20d44ba8966b9207
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-06-25 13:47:20 +0545

    fixed pictures

--- src/components/parallax/window/Window.jsx
+++ src/components/parallax/window/Window.jsx
@@ -7,22 +7,27 @@ const Window = () => {
 
   const { scrollYProgress: scrollYProgress1 } = useScroll({
     target: windowRef,
-    offset: ["start start", "end start"],
+    offset: ["0px 0px", "100vh 0px"],
   });
 
   const { scrollYProgress: scrollYProgress2 } = useScroll({
     target: windowRef,
+    offset: ["start start", "end start"],
+  });
+
+  const { scrollYProgress: scrollYProgress3 } = useScroll({
+    target: windowRef,
     offset: ["start start", "end center"],
   });
 
-  const scale = useTransform(scrollYProgress1, [0, 1], [1, 10]);
-  const xStar = useTransform(scrollYProgress1, [0, 1], ["0%", "50%"]);
-  const yText = useTransform(scrollYProgress1, [0, 1], ["-40%", "60%"]);
-  const yPlanet = useTransform(scrollYProgress1, [0, 1], ["-40%", "60%"]);
-  const yMount = useTransform(scrollYProgress2, [0, 1], ["10%", "0%"]);
+  const scale = useTransform(scrollYProgress1, [0, 1], [1, 9]);
+  const xStar = useTransform(scrollYProgress2, [0, 1], ["0%", "50%"]);
+  const yText = useTransform(scrollYProgress2, [0, 1], ["-40%", "60%"]);
+  const yPlanet = useTransform(scrollYProgress2, [0, 1], ["-40%", "60%"]);
+  const yMount = useTransform(scrollYProgress3, [0, 1], ["10%", "0%"]);
 
   return (
-    <div className="container">
+    <div className="container" ref={windowRef}>
       <div className="starContainer">
         <motion.div className="stars" style={{ x: xStar }}></motion.div>
       </div>
@@ -30,7 +35,7 @@ const Window = () => {
         <motion.div className="planets" style={{ y: yPlanet }}></motion.div>
       </div>
 
-      <div className="wrapper" ref={windowRef}>
+      <div className="wrapper">
         <section className="windowSection">
           <motion.div className="window" style={{ scale }}></motion.div>
         </section>
