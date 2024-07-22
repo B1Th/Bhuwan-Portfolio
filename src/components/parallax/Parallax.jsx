4fe4cc56d43184435f0b8a2b83e2dacccb2e02f9
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-07-22 23:04:47 +0545

    fixed text animation 1.9

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -31,7 +31,7 @@ const Parallax = () => {
     const options = {
       root: null,
       rootMargin: "0px",
-      threshold: 0.7555,
+      threshold: 0.7599,
     };
 
     const observer = new IntersectionObserver((entries) => {
