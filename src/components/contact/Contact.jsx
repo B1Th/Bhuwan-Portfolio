2daad6aef86d1e1333cf1d4dca22c9a166f681a7
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-08-24 23:49:10 +0545

    customized go to top button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -148,12 +148,13 @@ const Contact = () => {
           </div>
         </motion.div>
 
-        <motion.button className="arrowContainer" whileTap={{ scale: 0.9 }}>
+        <button className="button">
           <a href="#Home">
-            <img className="arr1" src="/uparrow.png" alt="" />
-            <img className="arr2" src="/uparrow.png" alt="" />
+            <svg class="svgIcon" viewBox="0 0 384 512">
+              <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
+            </svg>
           </a>
-        </motion.button>
+        </button>
       </div>
     </>
   );
